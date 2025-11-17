4089e4259d8acca3e07e8cbc6a5ddbdd4a8e1cc1
Chris Ley <christopher.ley@gmail.com>
2025-11-17 22:53:01 +0100

    first commit

---
+++ terraform/aws_infra/ec2-bastion.tf
@@ -0,0 +1,47 @@
+
+#########
+# EC2 configuration
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = local.sshkey_name
+  public_key = "${var.public_ssh_key}"
+}
+
+resource "aws_instance" "ec2instance" {
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+  subnet_id              = module.vpc.public_subnets[0]
+  private_ip             = "10.0.4.10"
+  vpc_security_group_ids = ["${aws_security_group.ec2-bastion.id}"]
+
+  #checkov:skip=CKV_AWS_88
+  associate_public_ip_address = true
+
+  key_name = "${aws_key_pair.ssh_key.key_name}"
+
+  iam_instance_profile = aws_iam_instance_profile.vm_profile.name
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 20
+    volume_type           = "gp3"
+    encrypted             = true
+  }
+
+  # https://docs.bridgecrew.io/docs/bc_aws_general_31
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  depends_on = [
+    aws_key_pair.ssh_key,
+    aws_iam_instance_profile.vm_profile
+  ]
+
+  tags = {
+    Name    = local.ec2_bastion_name
+    owners  = "christopher.ley@gmail.com"
+  }
+}
---
+++ terraform/aws_infra/iam-vm-creator.tf
@@ -0,0 +1,48 @@
+###########################
+# IAM overly permissive policy
+###########################
+resource "aws_iam_role" "vm_creator" {
+  name = "vm_creator_overly_permissive"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    owner = "christopher.ley@gmail.com"
+  }
+}
+
+resource "aws_iam_role_policy" "overly_permissive" {
+  name = "AllowEverythingPolicy"
+  role = aws_iam_role.vm_creator.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action   = "*"
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+
+  tags = {
+    owner = "christopher.ley@gmail.com"
+  }
+}
+
+resource "aws_iam_instance_profile" "vm_profile" {
+  name = "vm_overly_perm_profile"
+  role = aws_iam_role.vm_creator.name
+
+  tags = {
+    owner = "christopher.ley@gmail.com"
+  }
+}
