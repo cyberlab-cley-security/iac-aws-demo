a911f52719f1286a1760c668899a5b9d98ccd53a
Chris Ley <christopher.ley@gmail.com>
2025-11-18 00:48:24 +0100

    Fix sg.tf for mongodb only from eks

--- terraform/aws_infra/sg.tf
+++ terraform/aws_infra/sg.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "ec2-bastion" {
     from_port         = 27017
     to_port           = 27017
     protocol          = "tcp"
-    security_groups = [module.eks.managed_node_group_sg_ids["one"]] #to allow only connection from EKS node group to MongoDB
+    security_groups   = [module.eks.node_security_group_id] #to allow only connection from EKS node group to MongoDB
   }
 
   egress {
