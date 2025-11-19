815ee0b0724c379985581f5c9e54d4141d733b14
Chris Ley <christopher.ley@gmail.com>
2025-11-19 13:39:52 +0100

    Fix & Optimize DevSecOps Pipeline - v2

--- scripts/check_s3_bucket.sh
+++ scripts/check_s3_bucket.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-bucket_name=$1-$2-s3-tfstate
-region=$3
+# Arguments
+NAME_PREFIX=$1
+ACTOR_PREFIX=$2
+REGION=$3
+TF_DIR=$4   # Chemin vers le module Terraform bucket_s3
+
+bucket_name="${NAME_PREFIX}-${ACTOR_PREFIX}-s3-tfstate"
 
 # Check if the bucket exists
-if aws s3api head-bucket --bucket $bucket_name 2>/dev/null; then
+if aws s3api head-bucket --bucket "$bucket_name" 2>/dev/null; then
     echo "The bucket '$bucket_name' exists."
-    echo "No need to create it, it will be used to store infrastructure tfstate which will be created later"
+    echo "No need to create it, it will be used to store infrastructure tfstate."
 else
     echo "The bucket '$bucket_name' does not exist."
     echo "'$bucket_name' Bucket S3 creation in progress..."
-    terraform -chdir="../terraform/bucket_s3" init
-    terraform -chdir="../terraform/bucket_s3" apply -var="name_prefix=$1" -var="actor=$2" -var="region=$3" -auto-approve
+
+    # Init and apply Terraform in the correct directory
+    terraform -chdir="$TF_DIR" init
+    terraform -chdir="$TF_DIR" apply \
+        -var="name_prefix=$NAME_PREFIX" \
+        -var="actor=$ACTOR_PREFIX" \
+        -var="region=$REGION" \
+        -auto-approve
 fi
